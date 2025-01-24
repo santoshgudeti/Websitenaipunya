54d0c474bcfdec507339412d933d517b61fd8b5d
santoshgudeti <santoshgudeti@gmail.com>
2025-01-24 17:35:17 +0530

    Deploy updated build with correct base path

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Websitenaipunya/', // Set the repository name here
+  base: '/Websitenaipunya/', // Repository name as base
 });
